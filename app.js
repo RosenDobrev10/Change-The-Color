066280d429bf347acc91a9b24709156e07bcfbad
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-09-23 18:13:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let colors = ['blue', 'red', 'yellow', 'green', 'black', 'white', 'grey', 'brown', 'orange', 'pink', 'purple', 'cyan', 'aquamarine'];
+const colors = ['blue', 'red', 'yellow', 'green', 'black', 'white', 'grey', 'brown', 'orange', 'pink', 'purple', 'cyan', 'aquamarine'];
 
-let button = document.getElementById('button');
+const button = document.getElementById('button');
 
 button.addEventListener('click', () => {
 
-    let randomColor = colors[Math.floor(Math.random() * colors.length)]
-    let body = document.getElementsByTagName('body')[0]
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]
+    const body = document.getElementsByTagName('body')[0]
 
     body.style.backgroundColor = randomColor;
 })
